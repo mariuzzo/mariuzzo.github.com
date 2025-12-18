dfd428dfb2272b136787a2d0a6564ba715ea6450
Rubens Mariuzzo <rubens@mariuzzo.com>
2025-12-18 17:06:40 -0400

    chore: update dependencies and devDependencies in package.json
    - Upgraded @fontsource packages to latest versions
    - Updated date-fns to version 4.1.0
    - Upgraded prismjs to version 1.30.0
    - Updated TypeScript related types and babel-plugin-styled-components
    - Upgraded gh-pages, husky, inquirer, lint-staged, and prettier to their latest versions
    - Added overrides for react-server-dom-webpack to ensure compatibility with React 18

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,11 @@
 import 'normalize.css/normalize.css'
 
-import { theme } from './src/styles/theme'
+import '@fontsource/source-sans-pro/300.css'
+import '@fontsource/source-sans-pro/400.css'
+import '@fontsource/source-sans-pro/600.css'
+import '@fontsource/source-sans-pro/900.css'
 
-import(`@fontsource/source-sans-pro/${theme.fontWeight.light}.css`)
-import(`@fontsource/source-sans-pro/${theme.fontWeight.regular}.css`)
-import(`@fontsource/source-sans-pro/${theme.fontWeight.semiBold}.css`)
-import(`@fontsource/source-sans-pro/${theme.fontWeight.black}.css`)
-
-import(`@fontsource/source-code-pro/${theme.fontWeight.regular}.css`)
-import(`@fontsource/source-code-pro/${theme.fontWeight.semiBold}.css`)
+import '@fontsource/source-code-pro/400.css'
+import '@fontsource/source-code-pro/600.css'
 
 import 'prismjs/themes/prism-okaidia.css'
